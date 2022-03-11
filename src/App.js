2b830f868055befe32459d7a8e6e4658e44801ad
Turan141 <darkhero-95@mail.ru>
2022-03-11 23:48:40 +0400

    added json server

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import HeaderComponent from './Components/Range-Sliders/Header/Header'
 import SMSSlider from './Components/Range-Sliders/Sliders/SMSSlider'
 import TotalComponent from './Components/Range-Sliders/TotalComponent'
 import SocialApps from './Components/Range-Sliders/SocialAndMessegners/SocialComponent'
-import MessengerComp from './Components/Range-Sliders/SocialAndMessegners/MessengersComponent'
-import ReactDOM from 'react-dom'
 
 import './Styles/Styles.scss'
 import './App.css'
@@ -16,15 +14,26 @@ function App() {
   const [payForTraffic, setPayForTraffic] = useState(0)
   const [payForSMS, setPayForSMS] = useState(0)
   const [payForMinutes, setPayForMinutes] = useState(0)
+  const [icons, setIcons] = useState('')
   const [totalSum, setTotalSum] = useState(
     payForMinutes + payForSMS + payForTraffic
   )
 
   useEffect(() => {
-    (function totalSumHandler() {
+    ;(function totalSumHandler() {
       return setTotalSum(payForMinutes + payForSMS + payForTraffic)
     })()
   }, [payForSMS, payForMinutes, payForTraffic])
+
+  useEffect(() => {
+    ;(function getData() {
+      fetch('https://api.jsonbin.io/b/622b9ff20618276743749675')
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => setIcons(data))
+    })()
+  }, [])
 
   return (
     <div className="MainAppDiv">
@@ -33,8 +42,8 @@ function App() {
       <TrafficSlider setPayForTraffic={setPayForTraffic} />
       <SMSSlider setPayForSMS={setPayForSMS} />
       <TotalComponent totalSum={totalSum} />
-      <SocialApps/>
-      <MessengerComp/>
+      {icons && <SocialApps type={'Соцсети'} icons={icons.social} />}
+      {icons && <SocialApps type={'Мессенджеры'} icons={icons.messengers} />}
     </div>
   )
 }
--- src/Components/Range-Sliders/SocialAndMessegners/SocialComponent.jsx
+++ src/Components/Range-Sliders/SocialAndMessegners/SocialComponent.jsx
@@ -1,40 +1,16 @@
 import '../../../Styles/Styles.scss'
+import { useState, useEffect } from 'react'
 
-
-const SocialApps = () => {
-  const icons = [
-    {
-      name: 'facebook',
-      icon: 'https://www.svgrepo.com/show/158427/facebook.svg',
-      price: 20,
-    },
-    {
-      name: 'vk',
-      icon: 'https://www.svgrepo.com/show/349554/vk.svg',
-      price: 20,
-    },
-    {
-      name: 'odnoklassniki',
-      icon: 'https://www.svgrepo.com/show/40575/odnoklassniki-logo.svg',
-      price: 20,
-    },
-    {
-      name: 'instagram',
-      icon: 'https://www.svgrepo.com/show/111199/instagram.svg',
-      price: 60,
-    },
-    {
-      name: 'tiktok',
-      icon: 'https://www.svgrepo.com/show/303260/tiktok-logo-logo.svg',
-      price: 60,
-    },
-  ]
-
+const SocialApps = ({ type, icons }) => {
   const iconsMap = icons.map((elem) => {
     return (
       <div className="iconMapsDiv" key={elem.name}>
         <div className="socialIcons">
-          <img src={elem.icon} alt={elem.name} />
+          <img
+            src={elem.icon}
+            alt={elem.name}
+            onClick={() => console.log(elem)}
+          />
         </div>
         <div className="socialPrice">
           <p>{elem.price}</p>
@@ -45,14 +21,11 @@ const SocialApps = () => {
 
   return (
     <>
-      <div className="socialHeaderName">
-        <h2>Соцсети</h2>
-      </div>
       <div className="socialMainDiv">
-      <div className="leftColumn">
-        <h1>Соцсети</h1>
-        <h2>Выберите пожалуйста желаемые соцсети</h2>
-      </div>
+        <div className="leftColumn">
+          <h1>{type}</h1>
+          <h2>Выберите пожалуйста желаемые соцсети</h2>
+        </div>
         <div className="socialSecondaryDiv">{iconsMap}</div>
       </div>
     </>
